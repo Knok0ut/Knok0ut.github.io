724f8b1089661e4f4de13aab8e87736b95403085
WangXurun <1180300904@stu.hit.edu.cn>
2021-11-08 17:58:47 +0800

    Site updated: 2021-11-08 17:58:46

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,10 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
         document.title = 'ヽ(●-`Д´-)ノ';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
         document.title = 'ヾ(Ő∀Ő3)ノ' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
